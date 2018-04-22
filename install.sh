8ef7592ede09150bbc5f57ae5f929ddb0d7bd38b
akakou <s16003@tokyo.kosen-ac.jp>
2018-04-22 18:26:42 +0900

    zplugのインストール方法を更新

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # install zplug
 cp -r ./HOME/.* ~/
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+export ZPLUG_HOME=~/.zplug
+git clone https://github.com/zplug/zplug $ZPLUG_HOME
 
 # install dein
 mkdir ~/.cache/dein
